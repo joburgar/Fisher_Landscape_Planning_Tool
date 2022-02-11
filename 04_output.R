44dae9ad9953ec3d8b3ba31de9a5fde4c860dd00
Burgar <Joanna.Burgar@gov.bc.ca>
2022-02-11 09:02:05 -0800

    Updating output for Boreal and Columbian populations when torus = TRUE and out = TRUE.

--- 04_output.R
+++ 04_output.R
@@ -24,7 +24,7 @@ tz = Sys.timezone() # specify timezone in BC
 
 # Load Packages
 list.of.packages <- c("tidyverse", "NetLogoR","nnls","lcmix","MASS","Cairo","PNWColors", "ggplot2",
-                      "sf","raster","rgdal","fasterize")
+                      "sf","raster","rgdal")
 # Check you have them and load them
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
@@ -36,26 +36,26 @@ source("00_IBM_functions.R")
 # Low, medium and high habitat = 0.5, 0.6, and 0.7 (same world set up, get actual values)
 # Low, medium and high survival = 0.7, 0.8, 0.9
 
-load("out/CI_noescape_rfsurv.RData")
-w1 <- CI_noescape_rfsurv[[1]]; w1$actual.prop.hab # 0.52
-w2 <- CI_noescape_rfsurv[[2]]; w2$actual.prop.hab # 0.67
-w3 <- CI_noescape_rfsurv[[3]]; w3$actual.prop.hab # 0.77
+load("out/CI_escape_rfsurv.RData")
+w1 <- CI_escape_rfsurv[[1]]; w1$actual.prop.hab # 0.49
+w2 <- CI_escape_rfsurv[[2]]; w2$actual.prop.hab # 0.59
+w3 <- CI_escape_rfsurv[[3]]; w3$actual.prop.hab # 0.70
 
-load("out/Boreal_noescape_rfsurv.RData")
+load("out/Boreal_escape_rfsurv.RData")
 
 ###--- plot the simulated landbases
-Cairo(file="out/BCI_noescape_w1.PNG",type="png",width=2200,height=2000,pointsize=12,bg="white",dpi=300)
-plot(w1$land, main="Simulated Landbase\n52% Suitable Habitat")
+Cairo(file="out/BCI_escape_w1.PNG",type="png",width=2200,height=2000,pointsize=12,bg="white",dpi=300)
+plot(w1$land, main="Simulated Landbase\n49% Suitable Habitat")
 points(w1$t0, pch = w1$t0$shape, col = of(agents = w1$t0, var = "color"))
 dev.off()
 
-Cairo(file="out/BCI_noescape_w2.PNG",type="png",width=2200,height=2000,pointsize=12,bg="white",dpi=300)
-plot(w2$land, main="Simulated Landbase\n67% Suitable Habitat")
+Cairo(file="out/BCI_escape_w2.PNG",type="png",width=2200,height=2000,pointsize=12,bg="white",dpi=300)
+plot(w2$land, main="Simulated Landbase\n59% Suitable Habitat")
 points(w2$t0, pch = w2$t0$shape, col = of(agents = w2$t0, var = "color"))
 dev.off()
 
-Cairo(file="out/BCI_noescape_w3.PNG",type="png",width=2200,height=2000,pointsize=12,bg="white",dpi=300)
-plot(w3$land, main="Simulated Landbase\n77% Suitable Habitat")
+Cairo(file="out/BCI_escape_w3.PNG",type="png",width=2200,height=2000,pointsize=12,bg="white",dpi=300)
+plot(w3$land, main="Simulated Landbase\n70% Suitable Habitat")
 points(w3$t0, pch = w3$t0$shape, col = of(agents = w3$t0, var = "color"))
 dev.off()
 
@@ -98,7 +98,7 @@ colnames(B_ABM.df) <- colnames(CI_ABM.df) <- c("Run","TimeStep","NewCount","Sim"
 a=1
 b=2300
 for(i in 4:6){
-  CI_ABM.df[a:b,] <- sim_output(sim_out=CI_noescape_rfsurv, sim_order=i, numsims=100)
+  CI_ABM.df[a:b,] <- sim_output(sim_out=CI_escape_rfsurv, sim_order=i, numsims=100)
   a=a+2300
   b=b+2300
 }
@@ -108,12 +108,12 @@ for(i in 4:6){
 a=1
 b=2300
 for(i in 4:6){
-  B_ABM.df[a:b,] <- sim_output(sim_out=Boreal_noescape_rfsurv, sim_order=i, numsims=100)
+  B_ABM.df[a:b,] <- sim_output(sim_out=Boreal_escape_rfsurv, sim_order=i, numsims=100)
   a=a+2300
   b=b+2300
 }
 
-CI_ABM.df$Pop <- "Central Interior"
+CI_ABM.df$Pop <- "Columbian"
 B_ABM.df$Pop <- "Boreal"
 
 ###---
@@ -134,7 +134,15 @@ se <- function(x) sqrt(var(x)/length(x))
 ABM.TS.se <- ABM.df %>% dplyr::select(-Run) %>% pivot_wider(names_from=TimeStep, values_from=NewCount, values_fn=se)
 ABM.TS.se$Param <- "SE"
 
-ABM.TS <- rbind(ABM.TS.mean, ABM.TS.se)
+LCL <- function(x) quantile(x, probs=0.05)
+ABM.TS.LCL <- ABM.df %>% dplyr::select(-Run) %>% pivot_wider(names_from=TimeStep, values_from=NewCount, values_fn=LCL)
+ABM.TS.LCL$Param <- "LCL"
+
+UCL <- function(x) quantile(x, probs=0.95)
+ABM.TS.UCL <- ABM.df %>% dplyr::select(-Run) %>% pivot_wider(names_from=TimeStep, values_from=NewCount, values_fn=UCL)
+ABM.TS.UCL$Param <- "UCL"
+
+ABM.TS <- rbind(ABM.TS.mean, ABM.TS.se, ABM.TS.LCL, ABM.TS.UCL)
 
 ABM.TS.df <- ABM.TS %>% pivot_longer(cols = TimeStep_01:TimeStep_23,names_to = "TimeStep",values_to = "Value" )
 ABM.TS.df <- ABM.TS.df %>% pivot_wider(names_from = Param, values_from = Value)
@@ -142,7 +150,40 @@ ABM.TS.df <- ABM.TS.df %>% pivot_wider(names_from = Param, values_from = Value)
 
 ABM.TS.use <- ABM.TS.df %>% filter(!TimeStep %in% c("TimeStep_01", "TimeStep_02"))
 
+
+ABM.TS.use$TimeStepNum <- as.numeric(substr(ABM.TS.use$TimeStep,10,11))
+
+pal_col <- pnw_palette(name="Starfish",n=7,type="discrete")
+
 sim.TS.plot <- ggplot(data = ABM.TS.use) +
+  theme_bw() + theme(strip.background = element_rect(fill = "white", colour = "white")) +
+  theme(panel.grid = element_blank())+
+  geom_ribbon(aes(x = TimeStepNum, ymin = LCL, ymax = UCL), fill = "#2c6184") +
+  geom_vline(xintercept = 11, col="darkgrey", lty=4) +
+  geom_line(aes(x = TimeStepNum, y = Mean)) +
+  # geom_errorbar(aes(x = TimeStepNum, y = Mean, ymin=Mean-SE, ymax= Mean+SE),
+  #               width=.2, position=position_dodge(0.05)) +
+  theme(axis.text.x = element_blank()) +
+  xlab("Time Step in 6 Month Intervals over 10 years") +
+  ylab("Number of Fishers Alive (Mean + 95% Confidence Intervals)")+
+  ggtitle("Simulations of Fisher Populations (100 Runs)\nBy Population and Proportion of Suitable Habitat")+
+  facet_wrap(~Pop+Prophab)
+
+sim.TS.plot
+
+#- Plot
+
+Cairo(file="out/BCI_sim_escape.TS.plot_CL.PNG",
+      type="png",
+      width=3000,
+      height=2200,
+      pointsize=15,
+      bg="white",
+      dpi=300)
+sim.TS.plot
+dev.off()
+
+sim.TS.plot_se <- ggplot(data = ABM.TS.use) +
   theme_bw() + theme(strip.background = element_rect(fill = "white", colour = "white")) +
   theme(panel.grid = element_blank())+
   geom_vline(xintercept = "TimeStep_11", col="grey", lty=4) +
@@ -155,18 +196,11 @@ sim.TS.plot <- ggplot(data = ABM.TS.use) +
   ggtitle("Simulations of Fisher Populations (100 Runs)\nBy Population and Proportion of Suitable Habitat")+
   facet_wrap(~Pop+Prophab)
 
-sim.TS.plot
+sim.TS.plot_se
 
 #- Plot
-
-Cairo(file="out/BCI_sim_noescape.TS.plot.PNG",
-      type="png",
-      width=3000,
-      height=2200,
-      pointsize=15,
-      bg="white",
-      dpi=300)
-sim.TS.plot
+Cairo(file="out/BCI_sim_escape.TS.plot_SE.PNG",type="png",width=3000,height=2200,pointsize=15,bg="white",dpi=300)
+sim.TS.plot_se
 dev.off()
 
 ################################################################################
@@ -186,17 +220,17 @@ nozerosims <- function(sim=Sim, pop=Pop){
   return(nozerosims)
 }
 
-Bph52_nozero <- nozerosims(sim="Sim04", pop="Boreal")
-Bph67_nozero <- nozerosims(sim="Sim05", pop="Boreal")
-Bph77_nozero <- nozerosims(sim="Sim06", pop="Boreal")
+Bph49_nozero <- nozerosims(sim="Sim04", pop="Boreal")
+Bph59_nozero <- nozerosims(sim="Sim05", pop="Boreal")
+Bph70_nozero <- nozerosims(sim="Sim06", pop="Boreal")
 
-CIph52_nozero <- nozerosims(sim="Sim04", pop="Central Interior")
-CIph67_nozero <- nozerosims(sim="Sim05", pop="Central Interior")
-CIph77_nozero <- nozerosims(sim="Sim06", pop="Central Interior")
+CIph49_nozero <- nozerosims(sim="Sim04", pop="Columbian")
+CIph59_nozero <- nozerosims(sim="Sim05", pop="Columbian")
+CIph70_nozero <- nozerosims(sim="Sim06", pop="Columbian")
 
 
-length(Bph52_nozero); length(Bph67_nozero); length(Bph77_nozero) # 52, 78, 83 reached 10 years
-length(CIph52_nozero); length(CIph67_nozero); length(CIph77_nozero) # none reached 10 years
+length(Bph49_nozero); length(Bph59_nozero); length(Bph70_nozero) # 59, 55, 84 reached 10 years
+length(CIph49_nozero); length(CIph59_nozero); length(CIph70_nozero) # none reached 10 years
 
 
 # find coordinates for each fisher at 11 year mark
@@ -265,48 +299,47 @@ raster_output <- function(sim_out=sim_out, sim_order=sim_order, sim_use=sim_use,
 
 ###--- For Sim04 in Boreal
 # Bph52_nozero
-rBph52 <- raster_output(sim_out=Boreal_noescape_rfsurv, sim_order=4, sim_use=Bph52_nozero,land=w1$land,
+rBph49 <- raster_output(sim_out=Boreal_escape_rfsurv, sim_order=4, sim_use=Bph49_nozero,land=w1$land,
                         TS=23, rExtent=rw1, rFun="sum",sFun="sum")
 
-rBph52
-plot(rBph52$raster)
+rBph49
+plot(rBph49$raster)
 
-length(Bph52_nozero) # 52
-w1$t0
-Cairo(file="out/rBph52_title.PNG", type="png", width=2200, height=2000,pointsize=15,bg="white",dpi=300)
-plot(rBph52$raster,
+length(Bph49_nozero) # 59
+# w1$t0
+Cairo(file="out/rBph49_title.PNG", type="png", width=2200, height=2000,pointsize=15,bg="white",dpi=300)
+plot(rBph49$raster,
      main="Estimated Fisher Abundance over 100 Simulations",
-     sub="Starting with 20 fishers and 52% suitable habitat\npredicted 27.9 \u00B1 2.6 (mean \u00B1 1 SE) fishers after 10 years.")
+     sub="Starting with 20 fishers and 49% suitable habitat\npredicted 26.3 \u00B1 2.8 (mean \u00B1 1 SE) fishers after 10 years.")
 dev.off()
 
 ###--- For Sim05 in Boreal
-# Bph67_nozero
-rBph67 <- raster_output(sim_out=Boreal_noescape_rfsurv, sim_order=5, sim_use=Bph67_nozero,land=w2$land,
+rBph59 <- raster_output(sim_out=Boreal_escape_rfsurv, sim_order=5, sim_use=Bph59_nozero,land=w2$land,
                         TS=23, rExtent=rw2, rFun="sum",sFun="sum")
 
-rBph67
-plot(rBph67$raster)
+rBph59
+plot(rBph59$raster)
 
-length(Bph67_nozero) # 78
-w2$t0
-Cairo(file="out/rBph67_title.PNG", type="png", width=2200, height=2000,pointsize=15,bg="white",dpi=300)
-plot(rBph67$raster,
+length(Bph59_nozero) # 55
+# w2$t0
+Cairo(file="out/rBph59_title.PNG", type="png", width=2200, height=2000,pointsize=15,bg="white",dpi=300)
+plot(rBph59$raster,
      main="Estimated Fisher Abundance over 100 Simulations",
-     sub="Starting with 20 fishers and 67% suitable habitat\npredicted 54.3 \u00B1 6.5 (mean \u00B1 1 SE) fishers after 10 years.")
+     sub="Starting with 20 fishers and 59% suitable habitat\npredicted 22.5 \u00B1 1.9 (mean \u00B1 1 SE) fishers after 10 years.")
 dev.off()
 
 ###--- For Sim06 in Boreal
-# Bph77_nozero
-rBph77 <- raster_output(sim_out=Boreal_noescape_rfsurv, sim_order=6, sim_use=Bph77_nozero,land=w3$land,
+# Bph70_nozero
+rBph70 <- raster_output(sim_out=Boreal_escape_rfsurv, sim_order=6, sim_use=Bph70_nozero,land=w3$land,
                         TS=23, rExtent=rw3, rFun="sum",sFun="sum")
 
-rBph77
-plot(rBph77$raster)
+rBph70
+plot(rBph70$raster)
 
-length(Bph77_nozero) # 83
-w3$t0
-Cairo(file="out/rBph77_title.PNG", type="png", width=2200, height=2000,pointsize=15,bg="white",dpi=300)
-plot(rBph77$raster,
+length(Bph70_nozero) # 84
+# w3$t0
+Cairo(file="out/rBph70_title.PNG", type="png", width=2200, height=2000,pointsize=15,bg="white",dpi=300)
+plot(rBph70$raster,
      main="Estimated Fisher Abundance over 100 Simulations",
-     sub="Starting with 20 fishers and 77% suitable habitat\npredicted 69.7 \u00B1 6.6 (mean \u00B1 1 SE) fishers after 10 years.")
+     sub="Starting with 20 fishers and 70% suitable habitat\npredicted 62.7 \u00B1 4.7 (mean \u00B1 1 SE) fishers after 10 years.")
 dev.off()
