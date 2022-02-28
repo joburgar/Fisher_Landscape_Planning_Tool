04b446a43828b99783dcca1ab5d3afe9b7c7edf4
Burgar <Joanna.Burgar@gov.bc.ca>
2022-02-28 13:30:21 -0800

    creating 'real_world' underlying scenarios.

--- 01_load.R
+++ 01_load.R
@@ -153,42 +153,113 @@ create_grid <- function (input=input, cellsize=cellsize){
 #   geom_sf(data=tmp$fishnet_grid_sf)+
 #   geom_sf_label(aes(label=grid_id))
 
-tmp <- create_grid(input=aoi.PEACE, cellsize=5500)
-ggplot(tmp$fishnet_grid_sf)+
-  geom_sf(data=tmp$fishnet_grid_sf)+
-  geom_sf_label(aes(label=grid_id))
-
-tmp2 <- tmp$fishnet_grid_sf %>% filter(grid_id %in% seq_len(150))
-
-ggplot(tmp2)+
-  geom_sf(data=tmp2)+
-  geom_sf_label(aes(label=grid_id))
-
-tmp3 <- tmp$fishnet_grid_sf %>%
-  filter(grid_id %in% c(61:68,78:85,96:103,111:118,131:138))
-  # filter(grid_id %in% c(1:16,22:29,38:45,59:66))
-
-tmp3$grid_id <- rownames(tmp3)
-
-ggplot(tmp3)+
-  geom_sf(data=tmp3)+
-  geom_sf_label(aes(label=grid_id))
-
-aoi <- tmp3
-st_write(aoi, dsn=paste0(getwd(),"/data/aoi_PTSA_example2.shp"), delete_layer=TRUE)
+# tmp <- create_grid(input=aoi.PEACE, cellsize=5500)
+# ggplot(tmp$fishnet_grid_sf)+
+#   geom_sf(data=tmp$fishnet_grid_sf)+
+#   geom_sf_label(aes(label=grid_id))
+#
+# tmp2 <- tmp$fishnet_grid_sf %>% filter(grid_id %in% seq_len(150))
+#
+# ggplot(tmp2)+
+#   geom_sf(data=tmp2)+
+#   geom_sf_label(aes(label=grid_id))
+#
+# tmp3 <- tmp$fishnet_grid_sf %>%
+#   filter(grid_id %in% c(61:68,78:85,96:103,111:118,131:138))
+#   # filter(grid_id %in% c(1:16,22:29,38:45,59:66))
+#
+# tmp3$grid_id <- rownames(tmp3)
+#
+# ggplot(tmp3)+
+#   geom_sf(data=tmp3)+
+#   geom_sf_label(aes(label=grid_id))
+#
+# aoi <- tmp3
+# st_write(aoi, dsn=paste0(getwd(),"/data/aoi_PTSA_example2.shp"), delete_layer=TRUE)
 # st_write(aoi, dsn=paste0(getwd(),"/data/aoi_QTSA_example2.shp"), delete_layer=TRUE)
 #####################################################################################
+###--- Create an example polygon, with potential cutblocks
+# Consolidated cutblock
+# 1: Harvested Areas of BC (Consolidated Cutblocks) (multiple, fgdb, wms, kml, pdf)
+# ID: b1b647a6-f271-42e0-9cd0-89ec24bce9f7
+# Name: harvested-areas-of-bc-consolidated-cutblocks-
+# aoi <- st_read(dsn=paste0(getwd(),"/data"), layer="cutblocks_sample") %>% st_transform(crs=26910)
+aoi <- st_make_grid(st_buffer(aoi %>% st_transform(crs=26910), dist=30000), n=1)
+aoi <- st_as_sf(aoi)
 
-###--- NOW BRING IN COVARIATES
-# aoi <- st_read(dsn=paste0(getwd(),"/data"), layer="aoi_QTSA_example")
+# aoi <- aoi %>% st_transform(crs=3005)
+# aoi.CUT <- retrieve_geodata_aoi(ID = "2ebb35d8-c82f-4a17-9c96-612ac3532d55")
+# aoi.CUT <- aoi.CUT %>% dplyr::select(id, FEATURE_ID, HARVEST_DATE, Area_km2)
+# aoi.CUT$HARVEST_Yr <- year(aoi.CUT$HARVEST_DATE)
+# aoi.CUT <- aoi.CUT %>% filter(!is.na(HARVEST_Yr))
+# aoi.CUT %>% dplyr::count(HARVEST_Yr) %>% st_drop_geometry()
+#
+# aoi.CUT$HARVEST_DECADE <- as.numeric(substr(aoi.CUT$HARVEST_Yr,3,3))
+# unique(aoi.CUT$HARVEST_DECADE)
+# aoi.CUT$HARVEST_DECADE <- case_when(aoi.CUT$HARVEST_DECADE==1 ~2010,
+#                                     aoi.CUT$HARVEST_DECADE==0 ~2000,
+#                                     aoi.CUT$HARVEST_DECADE==9 ~1990,
+#                                     aoi.CUT$HARVEST_DECADE==8 ~1980,
+#                                     aoi.CUT$HARVEST_DECADE==7 ~1970,
+#                                     aoi.CUT$HARVEST_DECADE==6 ~1960)
+# st_write(aoi.CUT, dsn=paste0(getwd(),"/data/aoi_CUT_example.shp"), delete_layer=TRUE)
+
+aoi.CUT <- st_read(dsn=paste0(getwd(),"/data"), layer="aoi_CUT_example")
+
+aoi.CUT %>% group_by(HARVEST_DECADE) %>% summarise(sum(Area_km2)) %>% st_drop_geometry()
+aoi.CUT %>% dplyr::count(HARVEST_DECADE) %>%  st_drop_geometry()
+
+ggplot()+
+  geom_sf(data=aoi.CUT, aes(fill=HARVEST_DECADE))
+
+# Run through canned examples with different configurations of cuts from 1970
+# actual cutblocks, but long enough ago to potentially be classified as 'suitable' by our hypothetical VRI test
+# first generate random numbers for each cutblock and select different sets per example
+# 420 cutblocks covering 55.6 km2 in 1980, try 4 examples
+# 1. all cutblocks harvested (420)
+# 2. half cutblocks harvested (210) random selection
+# 3. half cutblocks harvested (210), in 'lower quality' (i.e., unsuitable) habitat
+# 4. half cutblocks harvested (210), in 'higher quality' (i.e., suitable) habitat
+
+# First create fishnet for entire area (aoi = buffer of 30 km2 around cutblocks and bounding box)
+# Then bring in VRI for aoi
+aoi.grid <- create_grid(input=aoi %>% st_transform(crs=26910), cellsize=5500)
+
+aoi.utm <- aoi.grid$aoi_utm
+aoi.grid <- aoi.grid$fishnet_grid_sf
+
+ggplot()+
+  geom_sf(data=aoi.grid)
+
+# add in population field
 aoi.Fpop <- st_read(dsn=paste0(getwd(),"/data"), layer="aoi.Fpop")
 aoi.Fpop$Fpop <- case_when(aoi.Fpop$Fpop=="Central Interior" ~ "Columbian",
                            TRUE ~ as.character(aoi.Fpop$Fpop))
 
-aoi <- st_join(aoi, aoi.Fpop %>% st_transform(crs=26910))
+aoi.utm <- st_join(aoi.utm, aoi.Fpop %>% st_transform(crs=26910))
+aoi.grid <- st_join(aoi.grid, aoi.Fpop %>% st_transform(crs=26910))
 
 ggplot()+
-  geom_sf(data=aoi, aes(fill=Fpop))
+  geom_sf(data=aoi.CUT %>% filter(HARVEST_DECADE==1970), aes(fill=HARVEST_Yr))+
+  # geom_sf(data=aoi.CUT %>% filter(HARVEST_DECADE==1980), aes(fill=HARVEST_Yr))+
+  geom_sf(data=aoi.utm, fill=NA)
+
+
+### WILL HAVE TO CREATE "FAKE" SUITABLE HABITAT TO START - I.E., MAKE MORE SUITABLE TO SHOW TOOL USE
+
+# Canned Example 1
+aoi.CUT$rndmrnk <- rank(round(runif(nrow(aoi.CUT), min=10000, max=99999)))
+# example 1 = all cutblocks
+canBEx1 <- aoi.CUT %>% filter(HARVEST_DECADE==1970)
+# example 2 = random half of cutblocks
+canBEx2 <- aoi.CUT %>% filter(HARVEST_DECADE==1980) %>% arrange(rndmrnk)
+canBEx2 <- canBEx2[1:210,]
+# example 3 = "unsuitable quality" cuts
+tmp <- st_join(aoi.CUT %>% filter(HARVEST_DECADE==1960), aoi.VRI %>% dplyr::select(TREE20_prop),left=TRUE, largest=TRUE)
+tmp %>% dplyr::count(TREE20_prop)
+
+# Choose
+
 
 
 ################################################################################
@@ -214,24 +285,9 @@ ggplot()+
 # https://catalogue.data.gov.bc.ca/
 
 # to see the downloaded gdb and shp files
-list.files("./data")
+# list.files("./data")
 
-aoi <- aoi %>% st_transform(crs=3005)
-
-# Timber Supply Area
-# cached from online as too slow through bcdata package
-# downloaded 2022-01-20
-# aoi.TSA <- retrieve_gdb_shp_aoi(dsn="./data/FADM_TSA", layer="FADM_TSA_polygon")
-# aoi.TSA %>% group_by(TSNMBRDSCR) %>% summarise(sum(Area_km2)) %>% st_drop_geometry()
-
-# VRI
-# https://www2.gov.bc.ca/assets/gov/farming-natural-resources-and-industry/forestry/stewardship/forest-analysis-inventory/data-management/standards/vegcomp_toc_data_dictionaryv5_2019.pdf
-# https://www2.gov.bc.ca/assets/gov/farming-natural-resources-and-industry/forestry/stewardship/forest-analysis-inventory/data-management/standards/vegcomp_poly_rank1_data_dictionaryv5_2019.pdf
-# TOO BIG! DOWNLOAD THROUGH BC GEODATA AND CACHE FOR LARGER AREAS
-# will need to either use smaller aoi or grab this once and save - will take WAY TOO LONG on an as need basis unless going to cell size
-
-# fgdb = "./data/VEG_COMP_POLY_AND_LAYER_2020.gdb"
-# List all feature classes in a file geodatabase
+aoi <- aoi.utm %>% st_transform(crs=3005)
 
 # vegetation data (VRI)
 # bcdc_search("VRI", res_format = "wms")
@@ -247,8 +303,7 @@ aoi.VRI %>% filter(!is.na(PROJ_HEIGHT_1)) %>%
 # aoi.VRI$TREE20_prop <- ifelse(aoi.VRI$PROJ_HEIGHT_1>=20, 1,0)
 # aoi.VRI$TREE20_prop[is.na(aoi.VRI$TREE20_prop)] <- 0
 
-aoi
-sum(aoi$Area_km2)
+sum(aoi.VRI$Area_km2)
 # proportion of VRI with projected height >= 20 m
 aoi$TREE20_prop <- NA
 for(i in seq_len(nrow(aoi))){
@@ -260,7 +315,7 @@ for(i in seq_len(nrow(aoi))){
 }
 
 ggplot()+
-  geom_sf(data=aoi, aes(fill=TREE20_prop))
+  geom_sf(data=aoi.VRI, aes(fill=TREE20_prop))
 summary(aoi$TREE20_prop)
 
 aoi$Habitat <- ifelse(aoi$TREE20_prop>0.2, 1, 0)
@@ -294,3 +349,28 @@ dev.off()
 save(IBM_aoi, file="data/IBM_aoi_Pex2.RData")
 
 
+
+# rm(aoi.Fpop); rm(aoi.CUT) # for housekeeping, big file
+save.image("01_load.RData")
+# #####################################################################################
+
+# ###--- view OSM map area of interest (need to figure out how to add polygons...)
+aoi.latlon <- aoi %>% st_transform(crs=4326)
+st_bbox(aoi)
+
+LAT1 = st_bbox(aoi.latlon)[2] ; LAT2 = st_bbox(aoi.latlon)[4]
+LON1 = st_bbox(aoi.latlon)[3] ; LON2 = st_bbox(aoi.latlon)[1]
+
+#our background map
+map <- openmap(c(LAT2,LON1), c(LAT1,LON2), zoom = NULL,
+               type = c("osm", "stamen-toner", "stamen-terrain","stamen-watercolor", "esri","esri-topo")[6],
+               mergeTiles = TRUE)
+
+## OSM CRS :: "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs"
+map.latlon <- openproj(map, projection = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+OpenStreetMap::autoplot.OpenStreetMap(map.latlon)
+ggplot()+
+  geom_sf(data=aoi.latlon)
+
+#####################################################################################
